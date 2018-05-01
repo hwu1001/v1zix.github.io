956a07740748e570eb0a34b1540f0ca7465dffed
Henry Wu <hywu925@gmail.com>
2018-04-30 18:45:21 -0700

    Update documentation in .js file

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         var $target = $(this.hash);
         // https://api.jquery.com/stop/
         // https://api.jquery.com/animate/
+        // Smooth scrolling influenced by this post: https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
         $('html, body').stop().animate({ scrollTop: $target.offset().top - 55 }, 550, 'swing');
     });
 });
