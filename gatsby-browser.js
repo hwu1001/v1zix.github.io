4a0920c073e2f70c8fd3a03f985a8f3ab510f749
Henry Wu <hywu925@gmail.com>
2019-07-07 22:58:51 -0700

    Change PrismJS theme to Okaidia, add line numbers to code, import custom css for line highlights

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
+require('prismjs/themes/prism-okaidia.css');
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+require('./src/styles/global.css');
 
 exports.onClientEntry = () => {};
